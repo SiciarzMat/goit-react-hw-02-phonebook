a7e3128c9f1add66918449e7e56881a90796a81b
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-03-25 19:35:15 +0100

    form fixed

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -67,6 +67,7 @@ export class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            autocomplete="off"
             onChange={this.handleChange}
           />
         </StyledLabel>
@@ -79,6 +80,7 @@ export class ContactForm extends Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            autocomplete="off"
             onChange={this.handleChange}
           />
         </StyledLabel>
